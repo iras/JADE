3ad17e94331d0381279af48958ad85d6424e85ef
Ivano Ras <ivano.ras@gmail.com>
2012-11-08 13:48:29 +0000

    (1) wired each cluster page's remove button (2) refactored module View0

--- src/View0.py
+++ src/View0.py
@@ -9,11 +9,6 @@ from PyQt4.QtGui import *
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    _fromUtf8 = lambda s: s
-
 
 
 class CustomGraphicsView (QGraphicsView):
@@ -55,49 +50,50 @@ class View (QFrame):
         self.graphicsView.setViewportUpdateMode (QGraphicsView.SmartViewportUpdate)
         #self.graphicsView.setMouseTracking(True)
         
-        # toolbox definition
+        # toolbox definition + group page definition
         sizePolicy = QtGui.QSizePolicy (QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Expanding)
-        self.setObjectName (_fromUtf8 ("Form"))
+        self.setObjectName ('Form')
         self.resize (400, 300)
         self._toolBox = QtGui.QToolBox (self)
         self._toolBox.setGeometry(QtCore.QRect (0, 0, 131, 301))
         self._toolBox.setFont (self.font)
-        self._toolBox.setObjectName (_fromUtf8("_toolBox"))
+        self._toolBox.setObjectName ('_toolBox')
         self.groupCluster = QtGui.QWidget ()
         self.groupCluster.setGeometry (QtCore.QRect(0, 0, 91, 241))
-        self.groupCluster.setObjectName (_fromUtf8 ("groupCluster"))
+        self.groupCluster.setObjectName ('groupCluster')
         self.groupLineEdit = QtGui.QLineEdit (self.groupCluster)
         self.groupLineEdit.setGeometry (QtCore.QRect (2, 20, 71, 16))
-        self.groupLineEdit.setObjectName (_fromUtf8 ("groupLineEdit"))
+        self.groupLineEdit.setObjectName ('groupLineEdit')
         self.label = QtGui.QLabel (self.groupCluster)
         self.label.setGeometry (QtCore.QRect (4, 6, 62, 16))
         self.label.setFont (self.font)
-        self.label.setObjectName (_fromUtf8("label"))
+        self.label.setObjectName ('label')
         self.label_2 = QtGui.QLabel (self.groupCluster)
         self.label_2.setGeometry (QtCore.QRect (4, 40, 62, 16))
         self.label_2.setFont (self.font)
-        self.label_2.setObjectName (_fromUtf8 ("label_2"))
+        self.label_2.setObjectName ('label_2')
         self.groupLineEdit_2 = QtGui.QLineEdit (self.groupCluster)
         self.groupLineEdit_2.setGeometry (QtCore.QRect(2, 54, 71, 16))
-        self.groupLineEdit_2.setObjectName (_fromUtf8("groupLineEdit_2"))
+        self.groupLineEdit_2.setObjectName ('groupLineEdit_2')
         self.pushButton = QtGui.QPushButton (self.groupCluster)
         self.pushButton.setGeometry (QtCore.QRect (2, 90, 71, 16))
         self.pushButton.setFont (self.font)
-        self.pushButton.setObjectName (_fromUtf8 ("pushButton"))
-        self._toolBox.addItem (self.groupCluster, _fromUtf8 (""))
+        self.pushButton.setObjectName ('pushButton')
+        self._toolBox.addItem (self.groupCluster, '')
         self._toolBox.setItemText (self._toolBox.indexOf (self.groupCluster), QtGui.QApplication.translate ("Form", "Group", None, QtGui.QApplication.UnicodeUTF8))
         self.setWindowTitle (QtGui.QApplication.translate ("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText (QtGui.QApplication.translate ("Form", "name group", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText (QtGui.QApplication.translate ("Form", "group id", None, QtGui.QApplication.UnicodeUTF8))
         self.pushButton.setText (QtGui.QApplication.translate ("Form", "add cluster", None, QtGui.QApplication.UnicodeUTF8))
         self._toolBox.setSizePolicy (sizePolicy)
-        self.connect (self.pushButton, SIGNAL ("clicked()"), self.addclusterPage)
         
         # adding the first cluster - a cluster is always present.
         self._cluster_page = []
-        self.addclusterPage ()
+        self.cluster_page_index = 0
+        self.connect (self.pushButton, SIGNAL ("clicked()"), self.addClusterPage)  # connect the 'add cluster' button to the method generating new cluster pages.
+        self.addClusterPage ()
         
-        size = self.style ().pixelMetric (QStyle.PM_ToolBarIconSize)
+        #size = self.style ().pixelMetric (QStyle.PM_ToolBarIconSize)
         iconSize = QSize (16, 16) #QSize (size, size)
         
         zoomInIcon = QToolButton ()
@@ -124,16 +120,6 @@ class View (QFrame):
         zoomSliderLayout.addWidget (self.zoomSlider)
         zoomSliderLayout.addWidget (zoomOutIcon)
         
-        #self.addNodeBtn = QPushButton ()
-        #self.addNodeBtn.setText ("add node")
-        #self.addNodeBtn.setFont(font)
-        #self.addNodeBtn.setEnabled (True)
-        
-        #self.removeNodeBtn = QPushButton ()
-        #self.removeNodeBtn.setText ("remove node")
-        #self.addNodeBtn.setFont(font)
-        #self.removeNodeBtn.setEnabled (True)
-        
         self.printOutBtn = QPushButton()
         self.printOutBtn.setText ("print graph")
         self.printOutBtn.setFont (self.font)
@@ -144,18 +130,18 @@ class View (QFrame):
         self.loadNodesDescrpBtn.setFont (self.font)
         self.loadNodesDescrpBtn.setEnabled (True)
         
-        self.graphSaveBtn = QPushButton()
-        self.graphSaveBtn.setText ("save graph")
-        self.graphSaveBtn.setFont (self.font)
-        self.graphSaveBtn.setEnabled (True)
-        
         self.graphLoadBtn = QPushButton()
         self.graphLoadBtn.setText ("load graph")
         self.graphLoadBtn.setFont (self.font)
         self.graphLoadBtn.setEnabled (True)
         
+        self.graphSaveBtn = QPushButton()
+        self.graphSaveBtn.setText ("save graph")
+        self.graphSaveBtn.setFont (self.font)
+        self.graphSaveBtn.setEnabled (True)
+        
         self.resetButton = QToolButton ()
-        self.resetButton.setText ("0")
+        self.resetButton.setText ("r")
         self.resetButton.setFont (self.font)
         self.resetButton.setEnabled (False)
         
@@ -163,20 +149,18 @@ class View (QFrame):
         labelLayout = QHBoxLayout ()
         self.label = QLabel (name)
         self.label.setFont (self.font)
-        #labelLayout.addWidget (self.addNodeBtn)
-        #labelLayout.addWidget (self.removeNodeBtn)
         labelLayout.addWidget (self.loadNodesDescrpBtn)
         labelLayout.addWidget (self.graphSaveBtn)
         labelLayout.addWidget (self.graphLoadBtn)
         labelLayout.addWidget (self.printOutBtn)
-        labelLayout.addWidget (self.resetButton)
-        
-        labelLayout.addWidget (self.label)
+        #labelLayout.addWidget (self.label)
         labelLayout.addStretch ()
         
+        # top layout
         topLayout = QGridLayout ()
         topLayout.addLayout (labelLayout, 0, 1)
         topLayout.addWidget (self._toolBox, 1, 0)
+        topLayout.addWidget (self.resetButton, 0, 2)
         topLayout.addWidget (self.graphicsView, 1, 1)
         topLayout.addLayout (zoomSliderLayout, 1, 2)
         self.setLayout (topLayout)
@@ -193,35 +177,47 @@ class View (QFrame):
         
         self.printer = QPrinter (QPrinter.HighResolution)
     
-    def addclusterPage (self, event=None):
+    def addClusterPage (self, event=None):
+        
+        self.cluster_page_index += 1
         
         tmp_w = QWidget ()
         tmp_l = QLabel (tmp_w)
         tmp_e = QLineEdit (tmp_w)
         tmp_b = QPushButton (tmp_w)
-        self._cluster_page.append ([tmp_w, tmp_l, tmp_e, tmp_b])
-        tmp_index = len (self._cluster_page)
+        self._cluster_page.append ([self.cluster_page_index, tmp_w, tmp_l, tmp_e, tmp_b])
         
         tmp_w.setGeometry (QtCore.QRect (0, 0, 131, 241))
-        tmp_w.setObjectName (_fromUtf8 ('Cluster_'+str(tmp_index)))
+        tmp_w.setObjectName ('Cluster_'+str(self.cluster_page_index))
         
         tmp_l.setGeometry (QtCore.QRect (4, 6, 62, 16))
         tmp_l.setFont (self.font)
-        tmp_l.setObjectName (_fromUtf8('label_'+str(tmp_index)))
+        tmp_l.setObjectName ('label_'+str(self.cluster_page_index))
         
         tmp_e.setGeometry (QtCore.QRect (2, 20, 71, 16))
-        tmp_e.setObjectName (_fromUtf8 ('groupLineEdit_'+str(tmp_index)))
+        tmp_e.setObjectName ('groupLineEdit_'+str(self.cluster_page_index))
         
         tmp_b.setGeometry(QtCore.QRect (2, 50, 71, 16))
         tmp_b.setFont (self.font)
-        tmp_b.setObjectName (_fromUtf8 ('pushButton_'+str(tmp_index)))
+        tmp_b.setObjectName ('pushButton_'+str(self.cluster_page_index))
         
-        self._toolBox.addItem (tmp_w, _fromUtf8 ('cluster_page_'+str(tmp_index)))
-        self._toolBox.setItemText (self._toolBox.indexOf (tmp_w), QtGui.QApplication.translate ('Form', 'C_'+str(tmp_index), None, QtGui.QApplication.UnicodeUTF8))
+        self._toolBox.addItem (tmp_w, 'cluster_page_'+str(self.cluster_page_index))
+        self._toolBox.setItemText (self._toolBox.indexOf (tmp_w), QtGui.QApplication.translate ('Form', 'C_'+str(self.cluster_page_index), None, QtGui.QApplication.UnicodeUTF8))
         tmp_l.setText (QtGui.QApplication.translate ("Form", "name cluster", None, QtGui.QApplication.UnicodeUTF8))
         tmp_b.setText (QtGui.QApplication.translate ("Form", "delete", None, QtGui.QApplication.UnicodeUTF8))
-        self._toolBox.setCurrentIndex (tmp_index)
+        self._toolBox.setCurrentIndex (self.cluster_page_index)
         QtCore.QMetaObject.connectSlotsByName (self)
+        
+        # hook up the delete button (use a closure)
+        receiver = lambda : self.removeClusterPage (self.cluster_page_index)
+        self.connect (tmp_b, SIGNAL ("clicked()"), receiver)  # connect the 'add cluster' button to the method generating new cluster pages.
+    
+    def removeClusterPage (self, index_cluster):
+        
+        for item in self._cluster_page:
+            if int(item[0]) == index_cluster:
+                self._toolBox.removeItem (self._toolBox.currentIndex())
+                break
     
     def getGraphicsView (self): return self.graphicsView
     
@@ -260,18 +256,18 @@ class View (QFrame):
         
         aa = QFileDialog (self).getSaveFileName ()
         if aa != QString (u''): # it can be equal to QString(u'') when the user presses the Escape key, so in that circumstance, nothing is returned.
-            file = open (aa, 'w')
-            file.write (self.graph_view.delegateExport ())
-            file.close ()
+            file0 = open (aa, 'w')
+            file0.write (self.graph_view.delegateExport ())
+            file0.close ()
             print '\n*** file exported.\n'
     
     def importGraph (self):
                 
         aa = QFileDialog (self).getOpenFileName ()
         if aa != QString (u''): # it can be equal to QString(u'') when the user presses the Escape key, so in that circumstance, nothing is returned.
-            file = open (aa, 'r')
-            XML_content = file.read()
-            file.close ()
+            file0 = open (aa, 'r')
+            XML_content = file0.read()
+            file0.close ()
             self.graph_view.delegateImport (XML_content)
             print '\n*** file imported.\n'
     
@@ -287,8 +283,6 @@ class View (QFrame):
         
         self.graph_view = graph_view
         
-        #self.connect (self.addNodeBtn,       SIGNAL ("clicked()"), self.graph_view.addNodeAndTagPressBtnListener)
-        #self.connect (self.removeNodeBtn,    SIGNAL ("clicked()"), self.graph_view.removeNodeAndTagPressBtnListener)
         self.connect (self.printOutBtn,       SIGNAL ("clicked()"), self.printOutGraph)
         self.connect (self.loadNodesDescrpBtn,SIGNAL ("clicked()"), self.importNodesDescription)
         self.connect (self.graphSaveBtn,      SIGNAL ("clicked()"), self.exportGraph)
